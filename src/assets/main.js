11789a4be45e95f8dbca086518aace60ffcc0612
Your Name <you@example.com>
2021-12-26 16:51:24 -0300

    carrit

---
+++ src/assets/main.js
@@ -0,0 +1,58 @@
+const totalItems = document.getElementById('totalItems')
+const totalProducts = document.getElementById('totalProducts')
+const cartWrapper = document.getElementById('cartWrapper')
+
+let products = []
+
+const setCount = () => {
+    let totalCount = 0
+
+    for(let item in products){
+        totalCount += products[item].count
+    }
+
+    totalItems.innerText = totalCount.toString()
+    return totalCount
+}
+
+const totalPrice = () => {
+    let totalCart = 0
+
+    for(let item in products){
+        totalCart += products[item].price * products[item].count
+    }
+
+     totalProducts.innerText = `${totalCart}`
+    return totalCart
+}
+
+const productsList = () => {
+    cartWrapper.innerHTML = products.map(product => {
+        return `
+            <div class="cart-item">
+                <div class="cart-item-content">
+                    <span>${product.product}</span>
+                    <span>$ ${product.price}</span>
+                    <span>Cantidad: ${product.count}</span>
+                </div>
+            </div>
+        `
+    })
+}
+
+const addProduct = (product, price, count) => {
+    for(let item in products) {
+        if(products[item].product === product) {
+            products[item].count ++
+            setCount()
+            totalPrice()
+            productsList()
+            return
+        }
+    }
+
+    products.push({ product: product, price: price, count: count});
+    setCount()
+    totalPrice()
+    productsList()
+}
