a5a40d1e50f404d7447dd14f9a4be71cb9dbdb82
Your Name <you@example.com>
2021-12-27 23:27:19 -0300

    terminar reproductor de audio

--- src/assets/main.js
+++ src/assets/main.js
@@ -1,3 +1,5 @@
+//CARRITO
+
 const totalItems = document.getElementById('totalItems')
 const totalProducts = document.getElementById('totalProducts')
 const cartWrapper = document.getElementById('cartWrapper')
@@ -56,3 +58,50 @@ const addProduct = (product, price, count) => {
     totalPrice()
     productsList()
 }
+
+//REPRODUCTOR DE MUSICA
+
+const songList = [
+    {
+        title: "A Sentir",
+        file: "A Sentir.wav",
+    },
+    {
+        title: "Amore",
+        file: "Amore.wav",
+    },
+    {
+        title: "Morir",
+        file: "Morir.wav",
+    },
+    {
+        title: "Nimbo",
+        file: "Nimbo.wav",
+    },
+    {
+        title: "Volver a Nacer",
+        file: "Volver a Nacer.wav",
+    }
+]
+
+const songs = document.getElementById ('songs')
+
+
+
+const loadSongs = () => {
+    songList.forEach((song, index) => {
+        const li = document.createElement('li')
+        const link = document.createElement('a')
+        link.textContent = song.title
+        link.href = '#'
+        link.addEventListener('click',() => loadSong(index))
+        li.appendChild(link)
+        songs.appendChild(li)
+    })
+}
+
+const loadSong = (songIndex) => {
+    console.log(songIndex)
+}
+
+loadSongs()
